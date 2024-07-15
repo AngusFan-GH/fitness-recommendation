1a8e135b92381a3fa492c0da807b9642abbbdcfa
fanyingdong <fanyd@ccccit.com.cn>
2024-07-15 16:55:42 +0800

    ✨ feat(gender): 改为图片点选

--- pages/gender/gender.js
+++ pages/gender/gender.js
@@ -1,24 +1,31 @@
 const app = getApp();
 
 Page({
-  data: {
-    genderOptions: [
-      { name: "男生", value: "0" },
-      { name: "女生", value: "1" }
-    ],
-    gender: null
-  },
-  bindGenderChange: function(e) {
-    this.setData({
-      gender: e.detail.value
-    });
-  },
-  next: function() {
-    if (this.data.gender) {
-      app.globalData.gender = this.data.gender;
-      wx.navigateTo({
-        url: '/pages/age/age'
-      });
+    data: {
+        genderOptions: [{
+            name: "男生",
+            value: "0",
+            imageUrl: '/images/man.svg'
+        },
+        {
+            name: "女生",
+            value: "1",
+            imageUrl: '/images/woman.svg'
+        }
+        ],
+        gender: null
+    },
+    bindGenderChange: function (e) {
+        this.setData({
+            gender: e.currentTarget.dataset.gender
+        });
+    },
+    next: function () {
+        if (this.data.gender) {
+            app.globalData.gender = this.data.gender;
+            wx.navigateTo({
+                url: '/pages/age/age'
+            });
+        }
     }
-  }
 });
